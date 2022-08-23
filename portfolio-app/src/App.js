92483dae592338e2393997bbf2fe205d70afe185
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-08-22 20:17:23 -0400

    Components complited

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,20 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
+
+function App() {
+  return (
+    <div >
+      <NavBar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+    </div>
+  );
+}
+
+export default App;
