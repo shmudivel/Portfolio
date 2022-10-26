34899f845b31aaf8ac6f45ad5041af04314a9615
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-10-26 17:17:52 -0400

    dictionary added

--- portfolio-app/src/components/Work.jsx
+++ portfolio-app/src/components/Work.jsx
@@ -53,16 +53,18 @@ const Work = () => {
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+              <span className="ml-3 text-2xl font-bold text-white tracking-wider">
+                Shaft Dictionary (CN-RU-EN)
+                <p className="ml-3 my-2 text-xs">ReactJS, Tailwind, Firebase, i18n(npm)</p>
+              <p className="mx-3  text-xs" > Mining Dictionary for technical translators. Accessible in 3 languages, Chinese, English and Russian. Useful for technical translators.</p>
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="http://shmudivel.github.io/dictionary-cn-ru-en-firebase" target="_blank" rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/shmudivel/dictionary-cn-ru-en-firebase" target="_blank" rel="noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
