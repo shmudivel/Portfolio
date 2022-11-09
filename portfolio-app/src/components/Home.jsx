e6daeb5243625122c455536f9d4d527e2f24353e
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-11-09 12:46:54 -0500

    100% zoom top padding

--- portfolio-app/src/components/Home.jsx
+++ portfolio-app/src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     const { t } = useTranslation();
 
   return (
-    <div name="home" className="w-full h-screen bg-[#0a192f]">
+    <div name="home" className="w-full pt-10 h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-[#26ddcd]">{t("Hi, my name is")}</p>
