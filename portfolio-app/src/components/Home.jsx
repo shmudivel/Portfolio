7791b3a34aa69df2ea2084d0ba536a2510065bd2
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-12-13 15:15:32 -0500

    favicon & resume new link

--- portfolio-app/src/components/Home.jsx
+++ portfolio-app/src/components/Home.jsx
@@ -5,14 +5,10 @@ import { useTranslation } from "react-i18next";
 // Icons
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
-import {
-  FaGithub,
-  FaLinkedin,
-
-} from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Home = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <div name="home" className="w-full pt-10 h-screen bg-[#0a192f]">
@@ -26,8 +22,12 @@ const Home = () => {
           {t("I'm a Front-end Developer.")}
         </h2>
         <p className="text-[#8892b0] py-6 max-w-[800px]">
-          {t("Interested in projects that improve processes at all levels from large business scales to small daily activities. ")}
-          {t("I am a creative problem solver, as my diverse background helps me to introduce solutions from different perspectives.")}
+          {t(
+            "Interested in projects that improve processes at all levels from large business scales to small daily activities. "
+          )}
+          {t(
+            "I am a creative problem solver, as my diverse background helps me to introduce solutions from different perspectives."
+          )}
         </p>
         <div>
           {/* <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#26ddcd] hover:border-[#26ddcd]">
@@ -41,41 +41,41 @@ const Home = () => {
             <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 ">
               <li className="w-[160px] h-[60px] mr-0 flex justify-between items-center shadow-md shadow-[#040c16] hover:scale-110 duration-500 bg-blue-600">
                 <a
-                className="flex justify-between items-center w-10/12 text-gray-300"
+                  className="flex justify-between items-center w-10/12 text-gray-300"
                   href="https://www.linkedin.com/in/darkhan-iglikov-24a616157"
                   target="_blank"
                   rel="noreferrer"
                 >
-                   <FaLinkedin size={30} /> {t("Linkedin")}
+                  <FaLinkedin size={30} /> {t("Linkedin")}
                 </a>
               </li>
               <li className="w-[160px] h-[60px] flex justify-between items-center shadow-md shadow-[#040c16] hover:scale-110 duration-500 bg-[#333333]">
                 <a
-                className="flex justify-between items-center w-10/12 text-gray-300"
+                  className="flex justify-between items-center w-10/12 text-gray-300"
                   href="https://github.com/shmudivel"
                   target="_blank"
                   rel="noreferrer"
                 >
-                   <FaGithub size={30} /> Github
+                  <FaGithub size={30} /> Github
                 </a>
               </li>
               <li className="w-[160px] h-[60px] flex justify-between items-center shadow-md shadow-[#040c16] hover:scale-110 duration-500 bg-[#6fc2b0]">
                 <a
-                className="flex justify-between items-center w-9/12 text-gray-300"
-                href="mailto: shmudivel@gmail.com"
+                  className="flex justify-between items-center w-9/12 text-gray-300"
+                  href="mailto: shmudivel@gmail.com"
                 >
-                   <HiOutlineMail size={30} /> {t("Email")}
-                  </a>
+                  <HiOutlineMail size={30} /> {t("Email")}
+                </a>
               </li>
               <li className="w-[160px] h-[60px] flex justify-between items-center shadow-md shadow-[#040c16] hover:scale-110 duration-500 bg-[#565f69]">
                 <a
-                className="flex justify-between items-center w-10/12 text-gray-300"
-                href="https://www.linkedin.com/in/darkhan-iglikov-24a616157/overlay/1635505196130/single-media-viewer/"
-                target="_blank"
-                rel="noreferrer"
+                  className="flex justify-between items-center w-10/12 text-gray-300"
+                  href="https://drive.google.com/file/d/1-yHd_D_dkq4eATq8YBSQxrmWOfIQbrqb/view?usp=share_link"
+                  target="_blank"
+                  rel="noreferrer"
                 >
-                   <BsFillPersonLinesFill size={30} /> {t("Resume")}
-                  </a>
+                  <BsFillPersonLinesFill size={30} /> {t("Resume")}
+                </a>
               </li>
             </ul>
           </div>
